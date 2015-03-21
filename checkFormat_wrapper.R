30991e65d131621be0ce48e2446d0cc639580738
unknown <ET207099@IS222820.intra.cea.fr>
2015-03-21 22:31:02 +0100

    version updated and standardized

--- checkFormat_wrapper.R
+++ checkFormat_wrapper.R
@@ -1,9 +1,7 @@
 #!/usr/bin/Rscript --vanilla --slave --no-site-file
 
 
-library(batch) ## necessary for parseCommandArgs function
-args = parseCommandArgs(evaluate=FALSE)
-## interpretation of arguments given in command line as an R list of objects
+library(batch) ## parseCommandArgs
 
 source_local <- function(fname){
     argv <- commandArgs(trailingOnly = FALSE)
@@ -11,85 +9,122 @@ source_local <- function(fname){
     source(paste(base_dir, fname, sep="/"))
 }
 
-## Import the different functions
 source_local("checkFormat_script.R")
 
 
 
+rssVsGalL <- FALSE
 
-runExampleL <- FALSE
+if(rssVsGalL) { ## for running with R outside the Galaxy environment during development of the script
 
-if(runExampleL) { ## example of arguments
+    ## 'example' input dir
+    exaDirInpC <- "example/input"
 
-    ## prefix file in
-    pfxFilInpC <- "data/input/sacuri_"                       ## 'sacuri'
-    ## pfxFilInpC <- gsub("sacuri", "suvimax", pfxFilInpC)   ## 'suvimax'
-    ## pfxFilInpC <- gsub("sacuri", "bisphenol", pfxFilInpC) ## 'bisphenol'
+    argLs <- list(dataMatrix_in = file.path(exaDirInpC, "dataMatrix.tsv"),
+                  sampleMetadata_in = file.path(exaDirInpC, "sampleMetadata.tsv"),
+                  variableMetadata_in = file.path(exaDirInpC, "variableMetadata.tsv"))
 
-    argLs <- list(dataMatrix_in = paste0(pfxFilInpC, "dataMatrix.tsv"),
-                  sampleMetadata_in = paste0(pfxFilInpC, "sampleMetadata.tsv"),
-                  variableMetadata_in = paste0(pfxFilInpC, "variableMetadata.tsv"))
-
-    ## prefix file out
-    pfxFilOutC <- gsub("input", "output",
-                       gsub("dataMatrix.tsv", "", argLs[["dataMatrix_in"]]))
-    ## "output/sacuri_"
+    ## 'example' output dir
+    exaDirOutC <- gsub("input", "output", exaDirInpC)
 
     argLs <- c(argLs,
-               list(dataMatrix_out = paste0(pfxFilOutC, "dataMatrix.tsv"),
-                    sampleMetadata_out = paste0(pfxFilOutC, "sampleMetadata.tsv"),
-                    variableMetadata_out = paste0(pfxFilOutC, "variableMetadata.tsv"),
-                    information = paste0(pfxFilOutC, "information.txt")))
+               list(dataMatrix_out = file.path(exaDirOutC, "dataMatrix.tsv"),
+                    sampleMetadata_out = file.path(exaDirOutC, "sampleMetadata.tsv"),
+                    variableMetadata_out = file.path(exaDirOutC, "variableMetadata.tsv"),
+                    information = file.path(exaDirOutC, "information.txt")))
 
-    stopifnot(file.exists("data/output"))
+    stopifnot(file.exists(exaDirOutC))
 
-} else {
-
+} else
     argLs <- parseCommandArgs(evaluate=FALSE)
 
-}
+
+##------------------------------
+## Initializing
+##------------------------------
+
+## options
+##--------
+
+strAsFacL <- options()$stringsAsFactors
+options(stringsAsFactors = FALSE)
+
+## constants
+##----------
+
+modNamC <- "Check Format" ## module name
+
+## log file
+##---------
 
 sink(argLs[["information"]])
-on.exit(sink())
 
-## Script
-##-------
+cat("\nStart of the '", modNamC, "' Galaxy module call: ",
+    format(Sys.time(), "%a %d %b %Y %X"), "\n", sep="")
+
+
+##------------------------------
+## Computation
+##------------------------------
+
 
 resLs <- readAndCheckF(argLs[["dataMatrix_in"]],
                        argLs[["sampleMetadata_in"]],
                        argLs[["variableMetadata_in"]])
+datMN <- resLs[["datMN"]]
+samDF <- resLs[["samDF"]]
+varDF <- resLs[["varDF"]]
+chkL <- resLs[["chkL"]]
 
-if(resLs[["chkL"]]) {
 
-    datDF <- cbind.data.frame(dataMatrix = colnames(resLs[["datMN"]]),
-                              as.data.frame(t(resLs[["datMN"]])))
+##------------------------------
+## Ending
+##------------------------------
+
+
+if(chkL) {
+
+    datDF <- cbind.data.frame(dataMatrix = colnames(datMN),
+                              as.data.frame(t(datMN)))
     write.table(datDF,
                 file = argLs[["dataMatrix_out"]],
                 quote = FALSE,
                 row.names = FALSE,
                 sep = "\t")
 
-    samDF <- cbind.data.frame(sampleMetadata = rownames(resLs[["samDF"]]),
-                              resLs[["samDF"]])
+    samDF <- cbind.data.frame(sampleMetadata = rownames(samDF),
+                              samDF)
     write.table(samDF,
                 file = argLs[["sampleMetadata_out"]],
                 quote = FALSE,
                 row.names = FALSE,
                 sep = "\t")
 
-    varDF <- cbind.data.frame(variableMetadata = rownames(resLs[["varDF"]]),
-                              resLs[["varDF"]])
+    varDF <- cbind.data.frame(variableMetadata = rownames(varDF),
+                              varDF)
     write.table(varDF,
                 file = argLs[["variableMetadata_out"]],
                 quote = FALSE,
                 row.names = FALSE,
                 sep = "\t")
 
-    cat("Table formats are OK; enjoy your analyses!", sep="")
+    cat("\nTable formats are OK; enjoy your analyses!\n", sep="")
+
+    cat("\nEnd of the '", modNamC, "' Galaxy module call: ",
+        format(Sys.time(), "%a %d %b %Y %X"), "\n", sep="")
+
     sink()
+
 } else {
+
     sink()
     stop("Please check the generated 'information' file")
+
 }
+
+## closing
+##--------
+
+options(stringsAsFactors = strAsFacL)
 
 rm(list = ls())
